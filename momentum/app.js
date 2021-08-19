3a5a3ad6b436aea2040b2ba7235fde3d789cbb84
octo <dlqhdgkr456@gmail.com>
2021-08-19 20:27:38 +0900

    2.2 Variables

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,17 @@
-alert("hi");
+console.log(54545);         // integer
+console.log("lalala\n");      // string
+
+console.log(5 + 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log();
+
+// JS variables
+const a = 5;
+const b = 2;
+
+const myName = "Lee";   // camel case
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
