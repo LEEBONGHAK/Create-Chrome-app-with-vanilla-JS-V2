3083d27ad3f8bfe28af2a9c7fb7933d2e872ad80
octo <dlqhdgkr456@gmail.com>
2021-08-19 21:53:31 +0900

    2.3 const and let

--- momentum/app.js
+++ momentum/app.js
@@ -1,10 +1,16 @@
+// alert("hi");
+
+// type
 console.log(54545);         // integer
 console.log("lalala\n");      // string
 
+
+// console.log()
 console.log(5 + 2);
 console.log(5 * 2);
 console.log(5 / 2);
 console.log();
+
 
 // JS variables
 const a = 5;
@@ -15,3 +21,26 @@ const myName = "Lee";   // camel case
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("hello " + myName);
+console.log();
+
+
+// const and let
+/*
+    const - 변수의 값을 바꿀 수 없음, 일반적으로 사용
+    let - 변수의 값을 바꿀 수 있음, 필요에 따라 사용
+    var - use in old version JS, 위의 두 선언법을 쓰는 편이 더 좋음
+*/ 
+let c = 5;
+let name = "Kim";
+
+console.log(c);
+console.log("hello " + name);
+
+c = 7;
+name = "Lee";
+
+console.log(c);
+console.log("hello " + name);
+console.log();
+
