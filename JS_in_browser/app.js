e15e2fee947fe8bd166ed7ef6483f40892c13385
octo <dlqhdgkr456@gmail.com>
2021-08-23 20:35:29 +0900

    3.3 Events

--- JS_in_browser/app.js
+++ JS_in_browser/app.js
@@ -9,9 +9,15 @@ console.log(grab.innerText);
 const hellos = document.getElementsByClassName("hello");
 console.log(hellos);
 
-const div_hello = document.querySelector(".div_hello h1");     // element를 css 방식으로 검색 (가장 첫번째 element만 찾고 그 이상은 안찾음)
+const div_hello = document.querySelector(".div_hello h1");     // element를 css 방식(tag, .class, #id)으로 검색 (가장 첫번째 element만 찾고 그 이상은 안찾음)
 console.log(div_hello);
 
 const hellos2 = document.querySelectorAll("h1");    // html 내 모든 지정 tag들 검색하여 array로 반환
 console.log(hellos2);
 
+
+function handleGrabClick() {
+    console.log("Grab event is happened!");
+}
+
+document.addEventListener("click", handleGrabClick);
