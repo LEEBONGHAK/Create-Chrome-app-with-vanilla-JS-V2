215a43d3b13120e77960bde0256869e2b7917056
octo <dlqhdgkr456@gmail.com>
2021-08-23 20:21:05 +0900

    3.2 Searching For Elements

--- JS_in_browser/app.js
+++ JS_in_browser/app.js
@@ -1,5 +1,17 @@
-const title = document.getElementById("title");
-console.log(title.textContent);
+console.log(document.title);
 
-title.innerText = "Got you!"
-console.log(title.innerText);
+const grab = document.getElementById("grab");
+console.log(grab.textContent);
+
+grab.innerText = "Got you!"
+console.log(grab.innerText);
+
+const hellos = document.getElementsByClassName("hello");
+console.log(hellos);
+
+const div_hello = document.querySelector(".div_hello h1");     // element를 css 방식으로 검색 (가장 첫번째 element만 찾고 그 이상은 안찾음)
+console.log(div_hello);
+
+const hellos2 = document.querySelectorAll("h1");    // html 내 모든 지정 tag들 검색하여 array로 반환
+console.log(hellos2);
+
