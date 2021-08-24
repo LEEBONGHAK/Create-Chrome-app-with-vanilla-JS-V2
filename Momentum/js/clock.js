3d7ac75f29d6032162d966498b1cf027b1eb6065
octo <dlqhdgkr456@gmail.com>
2021-08-24 13:57:20 +0900

    5.2 PadStart

--- Momentum/js/clock.js
+++ Momentum/js/clock.js
@@ -2,7 +2,11 @@ const clock = document.querySelector("#clock");
 
 function getTime() {
     const date = new Date();
-    clock.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
+    // padStart(stringLength, filled) : 문자열 길이를 지정하고 길이보다 적은 문자열의 경우 filled에 지정한 문자를 앞에 채워줌
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerHTML = `${hours}:${minutes}:${seconds}`;
 }
 
 getTime();
