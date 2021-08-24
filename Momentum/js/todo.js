f71bcee3129e843aa6d9b659d5b13386fc0a90d2
octo <dlqhdgkr456@gmail.com>
2021-08-24 15:25:27 +0900

    7.1 Adding ToDos

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -4,10 +4,19 @@ const toDoList = document.getElementById("todo-list");
 
 const TODOS_LS = 'toDos';
 
+function paintToDo(newToDo) {
+    const li = document.createElement("li");      // html에 쓰일 tag생성
+    const span = document.createElement("span");      // html에 쓰일 tag생성
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
 function handletoDoSubmit(event) {
     event.preventDefault();
-    const newToDo = toDoInput.value;
-    toDoInput.value = "";
+    const newToDo = toDoInput.value;    // input value 저장
+    paintToDo(newToDo);
+    toDoInput.value = "";       // input value reset
 }
 
 toDoForm.addEventListener("submit", handletoDoSubmit);
