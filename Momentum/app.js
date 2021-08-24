17d9c047083af9e1608de0041a4217e1a87f3a62
octo <dlqhdgkr456@gmail.com>
2021-08-24 12:43:36 +0900

    4.3 Events part Two

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,10 +1,18 @@
 const loginForm = document.querySelector("#login_form");
 const loginInput = document.querySelector("#login_form input");
 
+const link = document.querySelector("a");
+
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     console.log(username);
 }
 
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick);
