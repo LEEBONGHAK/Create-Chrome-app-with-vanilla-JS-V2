44937084a2736a0cbda6255df20ba80ddd1345fd
octo <dlqhdgkr456@gmail.com>
2021-08-24 13:04:15 +0900

    4.5 Saving Username

--- Momentum/app.js
+++ Momentum/app.js
@@ -2,12 +2,14 @@ const loginForm = document.querySelector("#login_form");
 const loginInput = document.querySelector("#login_form input");
 const greeting = document.querySelector("#greeting"); 
 
-const HIDDEN_CLASSNAME = "hidden";
+const HIDDEN_CLASSNAME = "hidden", 
+    USER_LS = "currentUser";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USER_LS, username);
     greeting.innerHTML = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
