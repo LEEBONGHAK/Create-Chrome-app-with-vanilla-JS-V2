c2453952106d86205951415bad03b3c3a8364864
octo <dlqhdgkr456@gmail.com>
2021-08-24 12:56:55 +0900

    4.4 Getting Username

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,18 +1,15 @@
 const loginForm = document.querySelector("#login_form");
 const loginInput = document.querySelector("#login_form input");
+const greeting = document.querySelector("#greeting"); 
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
     const username = loginInput.value;
-    console.log(username);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerHTML = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLinkClick);
