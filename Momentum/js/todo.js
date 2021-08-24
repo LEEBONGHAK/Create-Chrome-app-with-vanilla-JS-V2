e0df2bdd40bf107c3f60272113799121d9261108
octo <dlqhdgkr456@gmail.com>
2021-08-24 16:05:24 +0900

    7.4 Loading To Dos part One

--- Momentum/js/todo.js
+++ Momentum/js/todo.js
@@ -6,12 +6,14 @@ const TODOS_LS = 'toDos';
 
 const toDos = [];
 
+
 function deleteToDo(event) {     // 버튼을 눌으면 해당 li와 저장된 local storage 데이터를 제거하는 함수
     const btn = event.target;   // .target : 'event'에 'target'은 선택한 것이 어떤 HTML을 갖는지 알려줌
     const li = btn.parentElement;  // .parentElement : 부모 tag를 불러옴
     
     li.remove()   // .remove() : 해당 tag를 제거
 }
+
 
 /* 
 object인 array 데이터를 JOSON.stringify를 이용해 string으로 변환하여
@@ -21,6 +23,7 @@ array를 local storage에 저장해 주는 함수
 function saveToDos() {      // toDos를 local storage에 저장하는 함수
     localStorage.setItem(TODOS_LS, JSON.stringify(toDos));  // stringify() : 변수 등을 문자열로 바꿈
 }
+
 
 function paintToDo(newToDo) {       // submit된 to do를 html에 추가해 보여주는 함수
     const li = document.createElement("li");      // html에 쓰일 tag생성
@@ -38,6 +41,7 @@ function paintToDo(newToDo) {       // submit된 to do를 html에 추가해 보�
     toDoList.appendChild(li);
 }
 
+
 function handletoDoSubmit(event) {  // submit event를 다루는 함수
     event.preventDefault();
     const newToDo = toDoInput.value;    // input value 저장
@@ -49,4 +53,12 @@ function handletoDoSubmit(event) {  // submit event를 다루는 함수
     paintToDo(newToDo);
 }
 
+
 toDoForm.addEventListener("submit", handletoDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_LS);  // local storage 내용을 가져옴
+
+if (saveToDos !== null) {    // when savedToDos exist in local storage
+    const parsedToDos = JSON.parse(savedToDos);  //  parse() : 문자열을 JSON으로 바꿈
+    parsedToDos.forEach((toDo) => console.log(toDo))  // .forEach() : array에 담겨있는 것들 각각에 한번씩 함수를 실행시킴
+}
