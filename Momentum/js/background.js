0fe4dab13ca7278a7495792fa82d4d2e55a14a9a
이봉학 <dlqhdgkr456@gmail.com>
2021-08-28 16:34:57 +0900

    Update background.js

--- Momentum/js/background.js
+++ Momentum/js/background.js
@@ -3,7 +3,7 @@ const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");      // html에 쓰일 tag생성
-bgImage.src = `Momentum/images/${chosenImage}`;      // img tag에 src 속성 값 추가
+bgImage.src = `/Momentum/images/${chosenImage}`;      // img tag에 src 속성 값 추가
 bgImage.classList.add("bgImage");
 
 document.body.appendChild(bgImage);        // appendChild() : 생성한 html tag를 가장 마지막에 추가
